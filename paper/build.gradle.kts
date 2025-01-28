4014d44e1c6f177da6c5abc40ebc37866e790b6d
Rafi <rafiei.default@gmail.com>
2025-01-28 14:46:05 +0330

    Add run-task plugin to run test server

--- paper/build.gradle.kts
+++ paper/build.gradle.kts
@@ -1,6 +1,7 @@
 plugins {
     alias(libs.plugins.paperweight) // Paperweight plugin for Minecraft server development
     alias(libs.plugins.bukkit.factory) // Bukkit resource factory plugin for generating plugin.yml at build time
+    alias(libs.plugins.run.paper) // The run-task plugin for running a test server and testing the plugin
 }
 
 repositories {
@@ -11,15 +12,17 @@ dependencies {
     paperweight.paperDevBundle(libs.versions.paper) // Paper development bundle
 }
 
+java {
+    toolchain.languageVersion = JavaLanguageVersion.of(17) // Use Java 17 toolchain
+}
+
+tasks {
+    runServer { minecraftVersion("1.18.2") } // Configure the Minecraft server version.
+}
+
 bukkitPluginYaml {
     name = "MCTemplate"
     main = "example.plugin.template.Template"
     authors.add("Author")
-    apiVersion = "1.18.2"
-}
-
-java {
-    toolchain {
-        languageVersion = JavaLanguageVersion.of(17) // Use Java 17 toolchain
-    }
+    apiVersion = "1.18"
 }
