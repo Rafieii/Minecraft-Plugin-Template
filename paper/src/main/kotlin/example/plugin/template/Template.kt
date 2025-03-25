f2dd7701bf7f87355a22c4e36bb7da542de6844b
Rafi <rafiei.default@gmail.com>
2025-03-25 23:11:04 +0330

    Create Main class instance

--- paper/src/main/kotlin/example/plugin/template/Template.kt
+++ paper/src/main/kotlin/example/plugin/template/Template.kt
@@ -3,12 +3,21 @@ package example.plugin.template
 import org.bukkit.plugin.java.JavaPlugin
 
 class Template: JavaPlugin() {
+    
+    companion object {
+        private lateinit var instance: Template
 
-    override fun onEnable() {
-        // Called when the plugin is enabled (initialize resources, register events/commands).
+        fun getInst(): Template {
+            return instance
+        }
     }
 
+    // Called when the plugin is enabled (initialize resources, register events/commands).
+    override fun onEnable() {
+        instance = this
+    }
+
+    // Called when the plugin is disabled (cleanup resources, save data).
     override fun onDisable() {
-        // Called when the plugin is disabled (cleanup resources, save data).
     }
 }
