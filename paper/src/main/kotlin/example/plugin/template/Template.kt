821f29270601082ce60d7dcb6be3639b10ff2fc5
Rafi <rafiei.default@gmail.com>
2025-03-26 19:23:42 +0330

    Implement Bstats inside main class (Commented out by default)

--- paper/src/main/kotlin/example/plugin/template/Template.kt
+++ paper/src/main/kotlin/example/plugin/template/Template.kt
@@ -6,6 +6,7 @@ class Template: JavaPlugin() {
     
     companion object {
         private lateinit var instance: Template
+//        private lateinit var metrics: Metrics
 
         fun getInst(): Template {
             return instance
@@ -15,9 +16,12 @@ class Template: JavaPlugin() {
     // Called when the plugin is enabled (initialize resources, register events/commands).
     override fun onEnable() {
         instance = this
+
+//        metrics = Metrics(getInst(), /* bStats ID Here */)
     }
 
     // Called when the plugin is disabled (cleanup resources, save data).
     override fun onDisable() {
+//        metrics.shutdown()
     }
 }
